09150e5c757605037511fd3a4f3c8b351d6c6e0e
Huici Pan <hp1100@nyu.edu>
2017-06-27 00:52:16 +0800

    Create 389. Find the Difference.java

---
+++ 389. Find the Difference.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int res = 0;
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        
+        for(int i = 0; i < cs.length; i++){
+            res = res ^ cs[i] ^ ct[i];
+        }
+        
+        res ^= ct[ct.length-1];
+        
+        return a[res - 'a'];
+    }
+}
