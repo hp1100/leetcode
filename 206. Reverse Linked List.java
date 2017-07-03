d60cc17dd9a5819ed72888b41f4302b3f002d5fe
Huici Pan <hp1100@nyu.edu>
2017-07-04 00:46:37 +0800

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//My solution(slow)
 public class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode dummy = new ListNode(0);
@@ -18,4 +19,31 @@ public class Solution {
         }
         return dummy.next;
     }
+}
+
+//Iterative
+public ListNode reverseList(ListNode head) {
+    /* iterative solution */
+    ListNode newHead = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
+
+//Recursive
+public ListNode reverseList(ListNode head) {
+    /* recursive solution */
+    return reverseListInt(head, null);
+}
+
+private ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if (head == null)
+        return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
 }
