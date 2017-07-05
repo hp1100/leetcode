ad8d29c046709c157eb10f647f7941f7d2d3fcef
Huici Pan <hp1100@nyu.edu>
2017-07-05 17:36:06 +0800

    Create 377. Combination Sum IV.java

---
+++ 377. Combination Sum IV.java
@@ -0,0 +1,23 @@
+public class Solution {
+    
+    private int dp[];
+    
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return helper(nums, target);
+    }
+    
+    public int helper(int[] nums, int target){
+        if(dp[target] != -1) return dp[target];
+        
+        int res = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= target) res += helper(nums, target - nums[i]);
+        }
+        dp[target] = res;
+    
+        return res;
+    }
+}
