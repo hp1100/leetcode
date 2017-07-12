3be70c80683772a2e2460bf6cca248fa61c9f904
Huici Pan <hp1100@nyu.edu>
2017-07-12 23:12:28 +0800

    Create 200. Number of Islands.java

---
+++ 200. Number of Islands.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j); //up
+        dfs(grid, i + 1, j); //down
+        dfs(grid, i, j - 1); //left
+        dfs(grid, i, j + 1); //right
+    }
+}
