ef9da90f868717d09b4e61a1aa21da4690d8910b
Huici Pan <hp1100@nyu.edu>
2017-07-31 01:37:34 +0800

    Create 15. 3Sum.java

---
+++ 15. 3Sum.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
+                int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+                while(lo < hi){
+                    if(nums[lo] + nums[hi] == sum){
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    }else if(nums[lo] + nums[hi] < sum){
+                        lo++;
+                    }else{
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
