a38af0e0f62675e12155da01322c10f68b48be96
Huici Pan <hp1100@nyu.edu>
2017-08-31 00:03:57 +0800

    Create 647. Palindromic Substrings.java

---
+++ 647. Palindromic Substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    private int count;
+    
+    public int countSubstrings(String s) {
+        if(s == null || s.length() == 0) return 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length;
+        }
+        
+        return count;
+    }
+    
+    public void extendPalindrome(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++; left--; right++;
+        }
+    }
+}
