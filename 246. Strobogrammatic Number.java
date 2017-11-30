ebd74a21064ab8bd04b0763304d2b8affca7e720
Huici Pan <hp1100@nyu.edu>
2017-11-29 22:28:46 -0500

    Create 246. Strobogrammatic Number.java

---
+++ 246. Strobogrammatic Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        if(num == null || num.length() == 0) return false;
+        int end = num.length()/2, i = 0, len = num.length()-1;
+        if(num.length() % 2 == 1 && num.charAt(end) != '0' && num.charAt(end) != '1' && num.charAt(end) != '8') 
+            return false;
+        while(i < end){
+            if(num.charAt(i) == '1' && num.charAt(len - i) == '1') {i++;continue;}
+            if(num.charAt(i) == '6' && num.charAt(len - i) == '9') {i++;continue;}
+            if(num.charAt(i) == '9' && num.charAt(len - i) == '6') {i++;continue;}
+            if(num.charAt(i) == '8' && num.charAt(len - i) == '8') {i++;continue;}
+            if(num.charAt(i) == '0' && num.charAt(len - i) == '0') {i++;continue;}
+            return false;
+        }
+        return true;
+    }
+}
