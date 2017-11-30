bbd0e50b19d69adf1c715a1dee903a5a70055053
Huici Pan <hp1100@nyu.edu>
2017-11-29 21:00:09 -0500

    Create 388. Longest Absolute File Path.java

---
+++ 388. Longest Absolute File Path.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // "dummy" length
+        int maxLen = 0;
+        for(String s:input.split("\n")){
+            int lev = s.lastIndexOf("\t")+1; // number of "\t"
+            while(lev+1 < stack.size()) stack.pop(); // find parent
+            int len = stack.peek()+s.length()-lev+1; // remove "/t", add"/"
+            stack.push(len);
+            // check if it is file
+            if(s.contains(".")) maxLen = Math.max(maxLen, len-1); 
+        }
+        return maxLen;
+    }
+}
