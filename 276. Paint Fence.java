67afb3064610ab6da991d5fcd1f03d57c536b9d7
Huici Pan <hp1100@nyu.edu>
2017-12-04 04:43:52 -0500

    Create 276. Paint Fence.java

---
+++ 276. Paint Fence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWays(int n, int k) {
+        if(n == 0 || k == 0) return 0;
+        if(n == 1) return k;
+        int diffColorCounts = k * (k - 1);
+        int sameColorCounts = k;
+        for(int i = 2; i < n; i++){
+            int temp = diffColorCounts;
+            diffColorCounts = (diffColorCounts + sameColorCounts) * (k - 1);
+            sameColorCounts = temp;
+        }
+        return diffColorCounts + sameColorCounts;
+    }
+}
