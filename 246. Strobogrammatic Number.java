cfd1997ad0a4e0e8aa3b9f84210f97657833d4e0
Huici Pan <hp1100@nyu.edu>
2017-12-03 23:06:19 -0500

    Update 246. Strobogrammatic Number.java

--- 246. Strobogrammatic Number.java
+++ 246. Strobogrammatic Number.java
@@ -15,3 +15,13 @@ class Solution {
         return true;
     }
 }
+
+// 4 line solution in java
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        for(int i = 0, j = num.length()-1; i <= j; i++, j--)
+            if(!"00 11 88 696".contains(num.charAt(i) + "" + num.charAt(j))) 
+                return false;
+        return true;
+    }
+}
