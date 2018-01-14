b0d7309272252dbdb7880975b5eccfaa427b5ee3
Huici Pan <hp1100@nyu.edu>
2018-01-13 19:01:25 -0500

    Update 437. Path Sum III.java

--- 437. Path Sum III.java
+++ 437. Path Sum III.java
@@ -30,3 +30,28 @@ public class Solution {
         isValidPath(root.right, sum - root.val);
     }
 }
+
+
+// clear
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+    
+    public int pathSumFrom(TreeNode node, int target){
+        if(node == null) return 0;
+        int left = pathSumFrom(node.left, target - node.val);
+        int right = pathSumFrom(node.right, target - node.val);
+        return node.val == target ? left + right + 1 : left + right;
+    }
+}
