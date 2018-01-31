b0ec65d514b85d84e00bdbd20be2ac9e54b7d680
Huici Pan <hp1100@nyu.edu>
2018-01-31 02:09:05 -0500

    Create 264. Ugly Number II.java

---
+++ 264. Ugly Number II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        Queue<Long> Q = new PriorityQueue<Long>();
+        HashSet<Long> inQ = new HashSet<Long>();
+        Long[] primes = new Long[3];
+        primes[0] = Long.valueOf(2);
+        primes[1] = Long.valueOf(3);
+        primes[2] = Long.valueOf(5);
+        for (int i = 0; i < 3; i++) {
+            Q.add(primes[i]);
+            inQ.add(primes[i]);
+        }
+        Long number = Long.valueOf(1);
+        for (int i = 1; i < n; i++) {
+            number = Q.poll();
+            for (int j = 0; j < 3; j++) {
+                if (!inQ.contains(primes[j] * number)) {
+                    Q.add(number * primes[j]);
+                    inQ.add(number * primes[j]);
+                }
+            }
+        }
+        return number.intValue();
+    }
+    
+}
