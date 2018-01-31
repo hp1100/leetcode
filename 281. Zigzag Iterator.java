32213dba2d74d2d9fd470917e00a58c007ecf630
Huici Pan <hp1100@nyu.edu>
2018-01-31 01:03:21 -0500

    Create 281. Zigzag Iterator.java

---
+++ 281. Zigzag Iterator.java
@@ -0,0 +1,24 @@
+public class ZigzagIterator {
+    
+    private Iterator<Integer> i, j, tmp;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        i = v2.iterator();
+        j = v1.iterator();
+    }
+
+    public int next() {
+        if (j.hasNext()) { tmp = j; j = i; i = tmp; }
+        return i.next();
+    }
+
+    public boolean hasNext() {
+        return i.hasNext() || j.hasNext();
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
