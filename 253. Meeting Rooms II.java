10193e625a409a81a4a7ace5868528eccfdb2071
Huici Pan <hp1100@nyu.edu>
2018-02-04 11:13:21 -0500

    Create 253. Meeting Rooms II.java

---
+++ 253. Meeting Rooms II.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0)
+            return 0;
+
+        // Sort the intervals by start time
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
+
+        // Use a min heap to track the minimum end time of merged intervals
+        PriorityQueue<Interval> heap = new PriorityQueue<Interval>((a, b) -> a.end - b.end);
+
+        // start with the first meeting, put it to a meeting room
+        heap.offer(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            // get the meeting room that finishes earliest
+            Interval interval = heap.poll();
+
+            if (intervals[i].start >= interval.end) {
+                // if the current meeting starts right after 
+                // there's no need for a new room, merge the interval
+                interval.end = intervals[i].end;
+            } else {
+                // otherwise, this meeting needs a new room
+                heap.offer(intervals[i]);
+            }
+
+            // don't forget to put the meeting room back
+            heap.offer(interval);
+        }
+
+        return heap.size();
+    }
+}
