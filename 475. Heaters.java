a75b546a54736715df4b71967cf4c82210e86766
Huici Pan <hp1100@nyu.edu>
2018-02-04 19:12:47 -0500

    Update 475. Heaters.java

--- 475. Heaters.java
+++ 475. Heaters.java
@@ -17,3 +17,28 @@ class Solution {
         return res;
     }
 }
+
+
+// 2018.2.4
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int max = 0;
+        for(int house: houses)
+            max = Math.max(max, binarySearch(house, heaters));
+        return max;
+    }
+    
+    private int binarySearch(int house, int[] heaters){
+        int lo = 0, hi = heaters.length-1;
+        if(house < heaters[lo]) return heaters[lo] - house;
+        if(house > heaters[hi]) return house - heaters[hi];
+        while(lo + 1 < hi){
+            int mid = (hi - lo) / 2 + lo;
+            if(heaters[mid] == house) return 0;
+            else if(heaters[mid] < house) lo = mid;
+            else hi = mid;
+        }
+        return heaters[hi] - house > house - heaters[lo] ? house - heaters[lo] : heaters[hi] - house;
+    }
+}
