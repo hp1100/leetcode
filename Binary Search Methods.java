8b05d87748e168d6566c2b1b85b58ccc0fe1af0b
Huici Pan <hp1100@nyu.edu>
2018-02-12 18:49:34 -0500

    Update Binary Search Methods.java

--- Binary Search Methods.java
+++ Binary Search Methods.java
@@ -1,45 +1,44 @@
-	// basic binary search
-  // return index if exist else return -1
-  // no duplicate
-  public int binarySearch(int[] nums, int target){
-		int lo = 0, hi = nums.length - 1;
-		while(lo <= hi){
-			int mid = (hi - lo) / 2 + lo;
-			if(nums[mid] == target){
-				return mid;
-			}else if(nums[mid] < target){
-				lo = mid + 1;
-			}else{
-				hi = mid - 1;
-			}
+// basic binary search
+// return index if exist else return -1
+// no duplicate
+public int binarySearch(int[] nums, int target){
+	int lo = 0, hi = nums.length - 1;
+	while(lo <= hi){
+		int mid = (hi - lo) / 2 + lo;
+		if(nums[mid] == target){
+			return mid;
+		}else if(nums[mid] < target){
+			lo = mid + 1;
+		}else{
+			hi = mid - 1;
 		}
-		return -1;
 	}
+	return -1;
+}
   
-  // find first element
-  private int binarySearch(int[] nums, int target){
-		int lo = 0, hi = nums.length - 1;
-		while(lo <= hi){
-			int mid = (hi - lo) / 2 + lo;
-			if(nums[mid] < target)
-				lo = mid + 1;
-			else
-				hi = mid - 1;
+// find first element
+private int binarySearch(int[] nums, int target){
+	int lo = 0, hi = nums.length - 1;
+	while(lo <= hi){
+		int mid = (hi - lo) / 2 + lo;
+		if(nums[mid] < target)
+			lo = mid + 1;
+		else
+			hi = mid - 1;
 			
-		}
-		return lo;
 	}
+	return lo;
+}
   
-  // find last element
-  private int binarySearch(int[] nums, int target){
-		int lo = 0, hi = nums.length - 1;
-		while(lo <= hi){
-			int mid = (hi - lo) / 2 + lo;
-			if(nums[mid] <= target)
-				lo = mid + 1;
-			else
-				hi = mid - 1;
-			
-		}
-		return lo;
+// find last element
+private int binarySearch(int[] nums, int target){
+	int lo = 0, hi = nums.length - 1;
+	while(lo <= hi){
+		int mid = (hi - lo) / 2 + lo;
+		if(nums[mid] <= target)
+			lo = mid + 1;
+		else
+			hi = mid - 1;
 	}
+	return lo;
+}
