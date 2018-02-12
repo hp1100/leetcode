f194dd2aa929db3930840df3b8ef4afd60a69b3e
Huici Pan <hp1100@nyu.edu>
2018-02-12 18:47:42 -0500

    Create Binary Search Methods.java

---
+++ Binary Search Methods.java
@@ -0,0 +1,45 @@
+	// basic binary search
+  // return index if exist else return -1
+  // no duplicate
+  public int binarySearch(int[] nums, int target){
+		int lo = 0, hi = nums.length - 1;
+		while(lo <= hi){
+			int mid = (hi - lo) / 2 + lo;
+			if(nums[mid] == target){
+				return mid;
+			}else if(nums[mid] < target){
+				lo = mid + 1;
+			}else{
+				hi = mid - 1;
+			}
+		}
+		return -1;
+	}
+  
+  // find first element
+  private int binarySearch(int[] nums, int target){
+		int lo = 0, hi = nums.length - 1;
+		while(lo <= hi){
+			int mid = (hi - lo) / 2 + lo;
+			if(nums[mid] < target)
+				lo = mid + 1;
+			else
+				hi = mid - 1;
+			
+		}
+		return lo;
+	}
+  
+  // find last element
+  private int binarySearch(int[] nums, int target){
+		int lo = 0, hi = nums.length - 1;
+		while(lo <= hi){
+			int mid = (hi - lo) / 2 + lo;
+			if(nums[mid] <= target)
+				lo = mid + 1;
+			else
+				hi = mid - 1;
+			
+		}
+		return lo;
+	}
