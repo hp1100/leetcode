868e707f8f51d0f1dcd0fd8301a4578b37df825c
Huici Pan <hp1100@nyu.edu>
2018-02-12 14:29:09 -0500

    Update 350. Intersection of Two Arrays II.java

--- 350. Intersection of Two Arrays II.java
+++ 350. Intersection of Two Arrays II.java
@@ -1,3 +1,4 @@
+// use hashmap - O(n)
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -18,5 +19,34 @@ class Solution {
             r[i] = res.get(i);
         
         return r;
+    }
+}
+
+
+// use two pointer when two arrays are sorted - O(nlgn)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        List<Integer> res = new ArrayList<>();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{ //nums1[i] == nums2[j]
+                res.add(nums1[i]);
+                i++;j++;
+            }
+        }
+        return listToArray(res);
+    }
+    
+    private int[] listToArray(List<Integer> list){
+        int[] arr = new int[list.size()];
+        for(int i = 0; i < list.size(); i++)
+            arr[i] = list.get(i);
+        return arr;
     }
 }
