0e54c09834a8d0f9bf988e55f8a04ecab353fd32
Huici Pan <hp1100@nyu.edu>
2018-02-22 16:52:51 -0500

    Update 654. Maximum Binary Tree.java

--- 654. Maximum Binary Tree.java
+++ 654. Maximum Binary Tree.java
@@ -7,6 +7,10 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// Recursive fucntion 
+// Time Complexity
+// avg : O(nlgn)  worst : O(n^2)
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         return helper(nums, 0, nums.length - 1, null);
@@ -31,4 +35,25 @@ class Solution {
         return res;
     }
     
+}
+
+
+// Iterative function
+// Time compleixty: O(n)
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                stack.peek().right = curr;
+            }
+            stack.push(curr);
+        }
+        
+        return stack.isEmpty() ? null : stack.removeLast();
+    }
 }
