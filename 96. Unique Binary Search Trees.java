980829961cface1bb684ecb9a4795d271f0190bf
Huici Pan <hp1100@nyu.edu>
2018-02-24 00:17:36 -0500

    Update 96. Unique Binary Search Trees.java

--- 96. Unique Binary Search Trees.java
+++ 96. Unique Binary Search Trees.java
@@ -1,24 +1,20 @@
-public class Solution {
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        // dp[2] = 2;
-        // dp[3] = 5;
-        if(n < 2)
-            return dp[n];
-        for(int i = 2; i <= n; i++){
-            int curResult = 0;
-            int j = 1;
-            while(j <= i/2){
-                curResult += dp[i-j] * dp[j-1] * 2;
-                j++;
-            }
-            if(i % 2 == 1){
-                curResult += dp[i-j] * dp[j-1];
-            }
-            dp[i] = curResult;
-        }
-        return dp[n];
+// G(n): the number of unique BST for a sequence of length n.
+// F(i, n), 1 <= i <= n: the number of unique BST, 
+// where the number i is the root of BST, and the sequence ranges from 1 to n.
+
+// 1. G(n) = F(1, n) + F(2, n) + ... + F(n, n). 
+// 2. F(i, n) = G(i-1) * G(n-i)	1 <= i <= n 
+// combine 1. and 2. => G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0) 
+
+public int numTrees(int n) {
+    int [] G = new int[n+1];
+    G[0] = G[1] = 1;
+    
+    for(int i=2; i<=n; ++i) {
+    	for(int j=1; j<=i; ++j) {
+    		G[i] += G[j-1] * G[i-j];
+    	}
     }
+
+    return G[n];
 }
