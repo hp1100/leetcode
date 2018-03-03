dec7c4718a3316a7775f13a72ba672cbe3862db5
Huici Pan <hp1100@nyu.edu>
2018-03-03 15:16:20 -0500

    Create 156. Binary Tree Upside Down.java

---
+++ 156. Binary Tree Upside Down.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) {
+            return root;
+        }
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;   // node 2 left children
+        root.left.right = root;         // node 2 right children
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
