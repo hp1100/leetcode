1ddb5d0ae0b2f4e6d09ac36a7da65c53e1a02e8b
Huici Pan <hp1100@nyu.edu>
2018-03-07 00:10:50 -0500

    Update 734. Sentence Similarity.java

--- 734. Sentence Similarity.java
+++ 734. Sentence Similarity.java
@@ -1,19 +1,17 @@
 class Solution {
-    public boolean areSentencesSimilar(String[] words1, String[] words2, String[][] pairs) {
-        if(words1 == null && words1 == null) return true;
-        if(words1 == null || words2 == null) return false;
-        if(words1.length != words2.length) return false;
-        Map<String, Set<String>> map = new HashMap<>();
-        for(String[] p: pairs){
-            if(!map.containsKey(p[0]))
-                map.put(p[0], new HashSet<>());
-            map.get(p[0]).add(p[1]);
-        }      
-        for(int i = 0; i < words1.length; i++){
-            if(words1[i].equals(words2[i]) || map.getOrDefault(words1[i], new HashSet<>()).contains(words2[i])
-              || map.getOrDefault(words2[i], new HashSet<>()).contains(words1[i])) 
-                continue;
-            return false;
+    public boolean areSentencesSimilar(
+            String[] words1, String[] words2, String[][] pairs) {
+        if (words1.length != words2.length) return false;
+
+        Set<String> pairset = new HashSet();
+        for (String[] pair: pairs)
+            pairset.add(pair[0] + "#" + pair[1]);
+
+        for (int i = 0; i < words1.length; ++i) {
+            if (!words1[i].equals(words2[i]) &&
+                    !pairset.contains(words1[i] + "#" + words2[i]) &&
+                    !pairset.contains(words2[i] + "#" + words1[i]))
+                return false;
         }
         return true;
     }
