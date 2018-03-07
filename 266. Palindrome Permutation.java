4f4f6a4d722c1f15fa066bea6c4a960fea222dca
Huici Pan <hp1100@nyu.edu>
2018-03-06 20:32:25 -0500

    Update 266. Palindrome Permutation.java

--- 266. Palindrome Permutation.java
+++ 266. Palindrome Permutation.java
@@ -9,4 +9,17 @@ class Solution {
                 count++;
         return count <= 1;
     }
+    
+    // use hashSet
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if (!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else 
+                set.remove(s.charAt(i));
+        }
+        return set.size() == 0 || set.size() == 1;
+    }
+
 }
