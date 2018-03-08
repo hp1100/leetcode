07f05ed58e6cd11042e00cc857db8ac95465f7dd
Huici Pan <hp1100@nyu.edu>
2018-03-08 17:28:30 -0500

    Update 331. Verify Preorder Serialization of a Binary Tree.java

--- 331. Verify Preorder Serialization of a Binary Tree.java
+++ 331. Verify Preorder Serialization of a Binary Tree.java
@@ -12,3 +12,34 @@ class Solution {
         return count * 2 - 1 == nodes.length && i == nodes.length;
     }
 }
+
+
+// use stack 
+
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        // using a stack, scan left to right
+        // case 1: we see a number, just push it to the stack
+        // case 2: we see #, check if the top of stack is also #
+        // if so, pop #, pop the number in a while loop, until top of stack is not #
+        // if not, push it to stack
+        // in the end, check if stack size is 1, and stack top is #
+        if (preorder == null) {
+            return false;
+        }
+        Stack<String> st = new Stack<>();
+        String[] strs = preorder.split(",");
+        for (int pos = 0; pos < strs.length; pos++) {
+            String curr = strs[pos];
+            while (curr.equals("#") && !st.isEmpty() && st.peek().equals(curr)) {
+                st.pop();
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+            st.push(curr);
+        }
+        return st.size() == 1 && st.peek().equals("#");
+    }
+}
