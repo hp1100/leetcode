eda7520198d5dcf1a6a3920f25c0ac7bbd6285c1
Huici Pan <hp1100@nyu.edu>
2018-03-08 16:25:16 -0500

    Update 520. Detect Capital.java

--- 520. Detect Capital.java
+++ 520. Detect Capital.java
@@ -26,3 +26,14 @@ class Solution {
         return true;
     }
 }
+
+// 5-line
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        for(char c : word.toCharArray())
+            if('Z' - c >= 0)
+                count++;
+        return count == 0 || count == word.length() || (count == 1 && 'Z' - word.charAt(0) >= 0); 
+    }
+}
