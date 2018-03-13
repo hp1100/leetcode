c0ad75219e6f4bd924a1a10a660924a190b3b9bb
Huici Pan <hp1100@nyu.edu>
2018-03-13 00:58:18 -0400

    Update 281. Zigzag Iterator.java

--- 281. Zigzag Iterator.java
+++ 281. Zigzag Iterator.java
@@ -22,3 +22,26 @@ public class ZigzagIterator {
  * ZigzagIterator i = new ZigzagIterator(v1, v2);
  * while (i.hasNext()) v[f()] = i.next();
  */
+
+// use Queue 
+public class ZigzagIterator {
+    
+    Queue<Iterator> queue;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        queue = new LinkedList<>();
+        if(!v1.isEmpty()) queue.add(v1.iterator());
+        if(!v2.isEmpty()) queue.add(v2.iterator());
+    }
+
+    public int next() {
+        Iterator<Integer> temp = queue.poll();
+        int res = temp.next();
+        if(temp.hasNext()) queue.offer(temp);
+        return res;
+    }
+
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+}
