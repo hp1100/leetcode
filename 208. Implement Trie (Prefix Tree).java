dafbed58371baeb658f3a1e1516cc1218e4e0295
Huici Pan <hp1100@nyu.edu>
2018-03-22 15:01:57 -0400

    Update 208. Implement Trie (Prefix Tree).java

--- 208. Implement Trie (Prefix Tree).java
+++ 208. Implement Trie (Prefix Tree).java
@@ -1,50 +1,63 @@
-class TrieNode {
-    public char val;
-    public boolean isWord; 
-    public TrieNode[] children = new TrieNode[26];
-    public TrieNode() {}
-    TrieNode(char c){
-        TrieNode node = new TrieNode();
-        node.val = c;
+class Trie {
+    
+    class TrieNode {
+        public char val;
+        public boolean isEnd;
+        public TrieNode[] children = new TrieNode[26];
+        public TrieNode() {}
+        public TrieNode(char ch) {
+            this.val = ch;
+        }
     }
-}
-
-public class Trie {
+    
     private TrieNode root;
+
+    /** Initialize your data structure here. */
     public Trie() {
-        root = new TrieNode();
-        root.val = ' ';
+        root = new TrieNode(' ');
     }
-
+    
+    /** Inserts a word into the trie. */
     public void insert(String word) {
-        TrieNode ws = root;
-        for(int i = 0; i < word.length(); i++){
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null){
-                ws.children[c - 'a'] = new TrieNode(c);
-            }
-            ws = ws.children[c - 'a'];
+            if(curr.children[c - 'a'] == null)
+                curr.children[c - 'a'] = new TrieNode(c);
+            curr = curr.children[c - 'a'];
         }
-        ws.isWord = true;
+        curr.isEnd = true;
     }
-
+    
+    /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        TrieNode ws = root; 
-        for(int i = 0; i < word.length(); i++){
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+            if(curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
         }
-        return ws.isWord;
+        return curr.isEnd;
     }
-
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        TrieNode ws = root; 
-        for(int i = 0; i < prefix.length(); i++){
+        TrieNode curr = root;
+        for(int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+            if(curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
         }
         return true;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
