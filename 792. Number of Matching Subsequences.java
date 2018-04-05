e5e450c7304b005cd9df4f6b7865a6a76935e3ed
Huici Pan <hp1100@nyu.edu>
2018-04-05 18:38:20 -0400

    Update 792. Number of Matching Subsequences.java

--- 792. Number of Matching Subsequences.java
+++ 792. Number of Matching Subsequences.java
@@ -12,3 +12,38 @@ public int numMatchingSubseq(String S, String[] words) {
     }
     return waiting[0].size();
 }
+
+
+class Solution {
+    public int numMatchingSubseq(String S, String[] words) {
+        Map<Character, List<Integer[]>> map = new HashMap<>();
+        int count = 0;
+        
+        for(int i = 0; i < words.length; i++) {
+            char head = words[i].charAt(0);
+            List<Integer[]> temp = map.getOrDefault(head, new ArrayList<>());
+            temp.add(new Integer[]{i, 1});
+            map.put(head, temp);
+        }
+        
+        for(char c : S.toCharArray()) {
+            if(!map.containsKey(c)) continue;
+            
+            List<Integer[]> advance = map.get(c);
+            map.remove(c);
+            
+            for(Integer[] a : advance) {
+                if(a[1] < words[a[0]].length()) {
+                    char head = words[a[0]].charAt(a[1]);
+                    List<Integer[]> temp = map.getOrDefault(head, new ArrayList<>());
+                    temp.add(new Integer[]{a[0], ++a[1]});
+                    map.put(head, temp);
+                }else{
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
