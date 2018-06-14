ae26f3fc236645adacd5903f7bf1c329a877069e
Huici Pan <hp1100@nyu.edu>
2018-06-14 02:33:22 -0400

    Update 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,10 +1,38 @@
+// dp O(n^2)
 class Solution {
     public boolean canJump(int[] nums) {
-        int max = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(i > max) return false;
-            max = Math.max(max, nums[i] + i);
+        int len = nums.length;
+        boolean[] f = new boolean[len];
+        
+        f[0] = true;
+        
+        for(int j = 1; j < len; j++) {
+            f[j] = false;
+            
+            // previous stone i before j
+            // last jump from i to j
+            for(int i = 0; i < j; i++) {
+                if(f[i] && i + nums[i] >= j) {
+                    f[j] = true;
+                    break;
+                }
+            }
         }
-        return true;
+        
+        return f[len - 1];
     }
+}
+
+// Greedy O(n)
+class Solution {
+  public boolean canJump(int[] nums) {
+    int leftMostGoodPosition = nums.length - 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      if (i + nums[i] >= leftMostGoodPosition) {
+        leftMostGoodPosition = i;
+      }
+    }
+    
+    return leftMostGoodPosition == 0;
+  }
 }
