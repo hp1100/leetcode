ca0f570b400a492686b99ea08748fbab38d06cb3
Huici Pan <hp1100@nyu.edu>
2018-06-18 00:29:44 -0400

    Update 300. Longest Increasing Subsequence.java

--- 300. Longest Increasing Subsequence.java
+++ 300. Longest Increasing Subsequence.java
@@ -1,3 +1,4 @@
+// 2017/07/11
 public class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
@@ -14,5 +15,23 @@ public class Solution {
             maxLen = Math.max(maxLen, len);
         }
         return maxLen;
+    }
+}
+
+
+// 2018/06/18 DP O(n^2)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length, res = 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], nums[i] > nums[j] ? dp[j] + 1 : 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 }
