c65c3d441127e51dc752b886eb82ea962ba473ed
Huici Pan <hp1100@nyu.edu>
2018-07-03 18:24:23 -0400

    Update 494. Target Sum.java

--- 494. Target Sum.java
+++ 494. Target Sum.java
@@ -1,3 +1,4 @@
+// O(2^n)
 public class Solution {
     
     int count = 0;
@@ -13,4 +14,27 @@ public class Solution {
         dfs(nums, S, sum + nums[idx], idx + 1);
         dfs(nums, S, sum - nums[idx], idx + 1);
     }
+}
+
+
+
+// O(Math.max(S, n))
+class Solution {
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        return sum < S || (S + sum) % 2 > 0 ? 0 : subsetSum(nums, (S + sum) >>> 1); 
+    }
+    
+    private int subsetSum(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int num : nums) 
+            for (int i = target; i >= num; i--)
+                dp[i] += dp[i - num];
+        return dp[target];
+    }
+
 }
