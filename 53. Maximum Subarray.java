27bc97f70ad55630f9480f3985409e93d7e94705
Huici Pan <hp1100@nyu.edu>
2018-07-03 17:54:38 -0400

    Update 53. Maximum Subarray.java

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -34,3 +34,15 @@ class Solution {
         return largestSum;
     }
 }
+
+// 2018.7.2 Space:O(1) do not need to record min value
+class Solution {
+    public int maxSubArray(int[] A) {
+        int maxEndingHere = A[0], maxSoFar = A[0];
+        for (int i = 1; i < A.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + A[i], A[i]);
+            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
