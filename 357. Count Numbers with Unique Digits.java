7b1b7e2555a336592f52278520ec4b3debe80a12
Huici Pan <hp1100@nyu.edu>
2018-07-07 18:09:25 -0400

    Create 357. Count Numbers with Unique Digits.java

---
+++ 357. Count Numbers with Unique Digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n == 0) return 1;
+        int res = 10;
+        int uniqueDigits = 9;
+        int availableNumber = 9;
+        while (n-- > 1 && availableNumber > 0) {
+            uniqueDigits = uniqueDigits * availableNumber--;
+            res += uniqueDigits;
+        }
+        return res;
+    }
+}
