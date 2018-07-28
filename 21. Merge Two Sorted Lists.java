1dc0f2fefec4eea2978cf4894c6c8d9dcb452dfa
Huici Pan <hp1100@nyu.edu>
2018-07-28 04:03:45 -0400

    Update 21. Merge Two Sorted Lists.java

--- 21. Merge Two Sorted Lists.java
+++ 21. Merge Two Sorted Lists.java
@@ -33,3 +33,40 @@ public class Solution {
         return dummy.next;
     }
 }
+
+// Just in one loop:
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        while (l1 != null || l2 != null) {
+            if (l2 == null || (l1 != null && l1.val <= l2.val)) {
+                node.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                node.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        return dummy.next;
+    }
+}
+
+// Recursive:
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
+
