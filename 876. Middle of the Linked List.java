7d92c1dadd42fa741de5d21398d34ca4e50c13f0
Huici Pan <hp1100@nyu.edu>
2018-07-29 03:32:16 -0400

    Create 876. Middle of the Linked List.java

---
+++ 876. Middle of the Linked List.java
@@ -0,0 +1,8 @@
+public ListNode middleNode(ListNode head) {
+	ListNode slow = head, fast = head;
+	while (fast != null && fast.next != null) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+	return slow;
+}
