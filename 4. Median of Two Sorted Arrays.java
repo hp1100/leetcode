4995352c823632bb00e83cd45272d05ae677fa41
Huici Pan <hp1100@nyu.edu>
2018-08-04 21:28:27 -0400

    Create 4. Median of Two Sorted Arrays.java

---
+++ 4. Median of Two Sorted Arrays.java
@@ -0,0 +1,35 @@
+class Solution {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { 
+            return findMedianSortedArrays(B,A); // make sure : m <= n
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && B[j-1] > A[i]){ 
+                iMin = i + 1; 
+            }
+            else if (i != 0 && j != n && A[i-1] > B[j]) {
+                iMax = i - 1; 
+            }
+            else {
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; } // odd
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0; // even
+            }
+        }
+        return 0.0;
+    }
+}
