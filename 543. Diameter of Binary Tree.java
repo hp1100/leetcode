0881edb20c84bdfaa097ba11a1aef781a496dbc4
Huici Pan <hp1100@nyu.edu>
2018-08-15 00:42:06 -0400

    Update 543. Diameter of Binary Tree.java

--- 543. Diameter of Binary Tree.java
+++ 543. Diameter of Binary Tree.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// O(n^2) 
 public class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
@@ -20,5 +21,24 @@ public class Solution {
     public int depthOfBinaryTree(TreeNode root){
         if(root == null) return 0;
         return Math.max(depthOfBinaryTree(root.left), depthOfBinaryTree(root.right)) + 1;
+    }
+}
+
+// O(n) 
+class Solution {
+    
+    int diameter = 0;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+    
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        diameter = Math.max(diameter, left + right);
+        return Math.max(left, right) + 1;
     }
 }
