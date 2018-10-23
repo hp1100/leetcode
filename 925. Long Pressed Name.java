17590f22913c0b41629608dcb37d977c1fe6bba4
Huici Pan <hp1100@nyu.edu>
2018-10-23 16:53:45 -0700

    Update 925. Long Pressed Name.java

--- 925. Long Pressed Name.java
+++ 925. Long Pressed Name.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        if (typed.length() < name.length()) return false;
-        if (name.length() == 0) return true;
+        // if (typed.length() < name.length()) return false;
+        // if (name.length() == 0) return true;
         if (name.charAt(0) != typed.charAt(0)) return false;
-        int idx = 1;
-        for (int i = 1; i < name.length(); i++, idx++) {
+        for (int i = 1, j = 1; i < name.length(); i++, j++) {
             if (name.charAt(i) != name.charAt(i - 1)) 
-                while (idx < typed.length() && typed.charAt(idx) == typed.charAt(idx - 1)) idx++;    
-            if (idx >= typed.length() || name.charAt(i) != typed.charAt(idx)) 
+                while (j < typed.length() && typed.charAt(j) == typed.charAt(j - 1)) j++;    
+            if (j >= typed.length() || name.charAt(i) != typed.charAt(j)) 
                 return false;
         }
         return true;
