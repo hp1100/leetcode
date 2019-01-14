c35e7fa641777b491a9ed491a0c24deef2bc003f
Huici Pan <hp1100@nyu.edu>
2019-01-14 15:52:21 -0800

    Create 973. K Closest Points to Origin.java

---
+++ 973. K Closest Points to Origin.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
