c43be0104437ae2f1a870d17d126c745764dab86
Huici Pan <hp1100@nyu.edu>
2019-05-04 22:59:33 -0700

    Update 1038. Binary Search Tree to Greater Sum Tree.java

--- 1038. Binary Search Tree to Greater Sum Tree.java
+++ 1038. Binary Search Tree to Greater Sum Tree.java
@@ -16,12 +16,23 @@ class Solution {
         return root;
     }
     
-    public int rightChildfs(TreeNode node) {
+    private int rightChildfs(TreeNode node) {
         if (node == null) return 0;
         int rightSum = rightChildfs(node.right);
         node.val += preSum;
         preSum = node.val;
         int leftSum = rightChildfs(node.left);
         return node.val;
+    }
+}
+
+// without private
+class Solution {
+    int pre = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root.right != null) bstToGst(root.right);
+        pre = root.val = pre + root.val;
+        if (root.left != null) bstToGst(root.left);
+        return root;
     }
 }
