dd98e1ebfacf3c97cc37576ea49a32d1fa463483
Huici Pan <hp1100@nyu.edu>
2019-05-08 00:59:30 -0700

    Update 1033. Moving Stones Until Consecutive.java

--- 1033. Moving Stones Until Consecutive.java
+++ 1033. Moving Stones Until Consecutive.java
@@ -17,3 +17,11 @@ class Solution {
         return new int[]{min, max};
     }
 }
+
+// 4 line solution
+public int[] numMovesStones(int a, int b, int c) {
+  int[] s = { a, b, c };
+  Arrays.sort(s);
+  if (s[2] - s[0] == 2) return new int[] { 0, 0 };
+  return new int[] { Math.min(s[1] - s[0], s[2] - s[1]) <= 2 ? 1 : 2, s[2] - s[0] - 2 };
+}
