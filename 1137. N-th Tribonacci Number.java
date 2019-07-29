20e0f3315e9c3dae0403b1ae0a3130473cac631b
Huici Pan <hp1100@nyu.edu>
2019-07-29 11:34:06 -0700

    Update 1137. N-th Tribonacci Number.java

--- 1137. N-th Tribonacci Number.java
+++ 1137. N-th Tribonacci Number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int tribonacci(int n) {
-        int sum = 2;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
-        q.offer(1);
-        q.offer(1);
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
+        int sum = 2;
+        Queue<Integer> q = new LinkedList<>(Arrays.asList(0, 1, 1));
         for (int i = 3; i < n; i++) {
             q.offer(sum);
             sum += sum - q.poll();
