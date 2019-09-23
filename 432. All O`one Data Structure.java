0b2f9cdc4b593e19fdcd312335872efc9374eb76
Huici Pan <hp1100@nyu.edu>
2019-09-23 01:00:02 -0700

    Create 432. All O`one Data Structure.java

---
+++ 432. All O`one Data Structure.java
@@ -0,0 +1,81 @@
+class AllOne {
+    
+    Map<String, Integer> counts;
+    Map<Integer, Set<String>> lists;
+    int min, max;
+    int preMin;
+
+    /** Initialize your data structure here. */
+    public AllOne() {
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+        preMin = Integer.MAX_VALUE;
+    }
+    
+    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+    public void inc(String key) {
+        if (!counts.containsKey(key)) {
+            counts.put(key, 1);
+            if (!lists.containsKey(1))
+                lists.put(1, new HashSet<>());
+            lists.get(1).add(key);
+            preMin = min;
+            min = Math.min(1, min);
+            max = Math.max(1, max);
+        } else {
+            int count = counts.get(key);
+            counts.put(key, count + 1);
+            lists.get(count).remove(key);
+            if (lists.get(count).size() == 0)
+                lists.remove(count);
+            if (!lists.containsKey(count + 1))
+                lists.put(count + 1, new HashSet<>());
+            lists.get(count + 1).add(key);
+            if (!lists.containsKey(min)) {
+                            preMin = min;
+
+                                min = count + 1;
+
+            }
+            max = Math.max(count + 1, max);
+        }
+    }
+    
+    /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+    public void dec(String key) {
+        if (!counts.containsKey(key)) return;
+        int count = counts.get(key);
+        if (count == 1) {
+            counts.remove(key);
+            lists.get(1).remove(key);
+            if (!lists.containsKey(min)) min = preMin;
+        } else {
+            counts.put(key, count - 1);
+            lists.get(count).remove(key);
+            if (!lists.containsKey(count - 1))
+                lists.put(count - 1, new HashSet<>());
+            lists.get(count - 1).add(key);
+            if (!lists.containsKey(max))
+                max = count - 1;
+            preMin = min;
+            min = Math.min(count - 1, min);
+        }
+        
+        if (lists.get(count).size() == 0)
+            lists.remove(count);
+    }
+    
+    /** Returns one of the keys with maximal value. */
+    public String getMaxKey() {
+        System.out.println("max:"+max);
+        return lists.containsKey(max) ? lists.get(max).iterator().next() : "";
+    }
+    
+    /** Returns one of the keys with Minimal value. */
+    public String getMinKey() {
+        System.out.println("min:"+min);
+        return lists.containsKey(min) ? lists.get(min).iterator().next() : lists.containsKey(preMin) ? lists.get(preMin).iterator().next() : getMaxKey();
+    }
+}
