ab0b404c9dd229da77385b6fd5b9ab132f77470b
Huici Pan <hp1100@nyu.edu>
2019-09-22 23:06:58 -0700

    Create 460. LFU Cache.java

---
+++ 460. LFU Cache.java
@@ -0,0 +1,55 @@
+class LFUCache {
+    
+    HashMap<Integer, Integer> vals;
+    HashMap<Integer, Integer> counts;
+    HashMap<Integer, LinkedHashSet<Integer>> lists;
+    int cap;
+    int min = -1;
+    public LFUCache(int capacity) {
+        this.cap = capacity;
+        this.vals = new HashMap<>();
+        this.counts = new HashMap<>();
+        this.lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>());
+    }
+    
+    public int get(int key) {
+        if(!vals.containsKey(key))
+            return -1;
+        int count = counts.get(key);
+        counts.put(key, count + 1);
+        lists.get(count).remove(key);
+        if(count == min && lists.get(count).size()==0)
+            min++;
+        if(!lists.containsKey(count + 1))
+            lists.put(count + 1, new LinkedHashSet<>());
+        lists.get(count + 1).add(key);
+        return vals.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if(cap<=0)
+            return;
+        if(vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key);
+            return;
+        } 
+        if(vals.size() >= cap) {
+            int evit = lists.get(min).iterator().next();
+            lists.get(min).remove(evit);
+            vals.remove(evit);
+        }
+        vals.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key);
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
