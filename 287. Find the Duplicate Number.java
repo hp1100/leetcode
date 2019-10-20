d0d6847be42a283d66c1f577b874362bbbcfe9ff
Huici Pan <hp1100@nyu.edu>
2019-10-20 01:27:14 -0700

    Update 287. Find the Duplicate Number.java

--- 287. Find the Duplicate Number.java
+++ 287. Find the Duplicate Number.java
@@ -8,3 +8,25 @@ class Solution {
         return idx;
     }
 }
+
+
+// 2019/10/20 Time: O(nlgn)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int lo = 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int p = lo, q = hi;
+            int mid = (hi - lo) / 2 + lo;
+            int count = 0;
+            for (int num : nums)
+                if (num <= mid)
+                    count++;
+            if (count > mid) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
