01199ced55f21fb347d6fc7ccb19cb110fb58ec7
Huici Pan <hp1100@nyu.edu>
2019-12-05 02:05:32 -0800

    Update 973. K Closest Points to Origin.java

--- 973. K Closest Points to Origin.java
+++ 973. K Closest Points to Origin.java
@@ -4,3 +4,28 @@ class Solution {
         return Arrays.copyOfRange(points, 0, K);
     }
 }
+
+
+
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0]*b[0]+b[1]*b[1]-a[0]*a[0]-a[1]*a[1]); //maximum heap
+        
+//         for (int[] point: points) {
+            
+//             pq.offer(point);
+//             if (pq.size() > K) pq.poll();
+//         }
+        
+//         int[][] res = new int[pq.size()][2];
+//         for (int i = 0; i < res.length; i++) {
+//             int[] point = pq.poll();
+//             res[i][0] = point[0];
+//             res[i][1] = point[1];
+//         }
+        
+//         return res;
+        
+        return Arrays.stream(points).sorted((a, b) -> a[0]*a[0]+a[1]*a[1]-b[0]*b[0]-b[1]*b[1]).limit(K).toArray(int[][]::new);
+    }
+}
