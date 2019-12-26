d512f8016f99beddbd8ff88fea515a816b33c6a5
Huici Pan <hp1100@nyu.edu>
2019-12-26 02:13:18 -0800

    Create 89. Gray Code.java

---
+++ 89. Gray Code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> rs=new ArrayList<Integer>();
+        rs.add(0);
+        for(int i = 0; i < n; i++){
+            int size = rs.size();
+            for(int k = size-1; k >= 0; k--)
+                rs.add(rs.get(k) | 1 << i);
+        }
+        return rs;
+    }
+}
