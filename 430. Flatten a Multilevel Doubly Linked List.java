037481706ca17ea7eb2a24bc1bc03ae2f15cc666
Huici Pan <hp1100@nyu.edu>
2020-01-01 02:10:50 -0800

    Update 430. Flatten a Multilevel Doubly Linked List.java

--- 430. Flatten a Multilevel Doubly Linked List.java
+++ 430. Flatten a Multilevel Doubly Linked List.java
@@ -36,3 +36,32 @@ class Solution {
         return node;
     }
 }
+
+
+// ------------------------------------------------------------------------------------------------------
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+class Solution {
+    
+    Node end = null;
+    
+    public Node flatten(Node head) {
+        if (head == null)
+            return end;
+        
+        end = flatten(head.next);
+        head.next = flatten(head.child);
+        if (head.next != null)
+            head.next.prev = head;
+        head.child = null;
+        return head;
+    }
+}
