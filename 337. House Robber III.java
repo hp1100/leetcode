61d3e2be68d55f7010132b752a95c799929c9ba4
Huici Pan <hp1100@nyu.edu>
2020-03-10 22:22:36 -0700

    Update 337. House Robber III.java

--- 337. House Robber III.java
+++ 337. House Robber III.java
@@ -28,3 +28,32 @@ public class Solution {
         return res;
     }
 }
+
+
+================================2020.03.10 done by myself=========================================
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        // res[0]: Maximum number could include node 
+        // res[1]: Maximum number exclude node
+        return Math.max(res[0], res[1]);
+    }
+    
+    private int[] dfs(TreeNode node) {
+        if (node == null) return new int[2];
+        
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+        
+        return new int[]{Math.max(left[1] + right[1] + node.val, left[0] + right[0]), left[0] + right[0]};
+    }
+}
