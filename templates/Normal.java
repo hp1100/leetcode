7b6da06fff7d1382e097e78a5503455ade789b1d
Huici Pan <hp1100@nyu.edu>
2020-04-26 03:47:19 -0700

    Create Normal.java

---
+++ templates/Normal.java
@@ -0,0 +1,18 @@
+// TreeMap/Treeset
+
+floorKey(Key key) // <=
+lowerKey(Key key) // <
+ceilingKey(Key key) // >=
+higherKey(key key) // >
+
+floorEntry(Key key) // <=
+lowerEntry(Key key) // <
+ceilingEntry(Key key) // >=
+higherEntry(key key) // >
+
+// Deque
+remveFist()
+addFisrt() 
+
+pollLast()
+addLast() / offerLast() / offer()
