c35e771551964c3a46ff59c4eb6f82765b6a7f2c
Huici Pan <hp1100@nyu.edu>
2020-06-21 03:14:45 -0700

    Create MST_Kruskal.java

---
+++ templates/MST_Kruskal.java
@@ -0,0 +1,63 @@
+class Solution {
+    public void costOfMst(int n, int[][] edges) { 
+        // Given a weighted undirected connected graph with n vertices numbered from 0 to n-1,
+        // an array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between nodes fromi and toi
+    
+        Arrays.sort(edges, (e1, e2)->Integer.compare(e1[2], e2[2]));
+      
+        return buildMST(n, edges);
+    }
+    
+    private int buildMST(int n, int[][] edges){
+        UnionFind uf = new UnionFind(n);
+        int cost = 0;
+        
+        for(int[] edge : edges){
+            if(uf.union(edge[0], edge[1])){
+                cost += edge[2];
+            }
+        }
+        return uf.count == 1? cost : Integer.MAX_VALUE;
+    }
+}
+
+class UnionFind{
+    final int[] parents;
+    int count;
+    
+    public UnionFind(int n){
+        this.parents = new int[n];
+        reset();
+    }
+    
+    public void reset(){
+        for(int i =0;i<parents.length;i++){
+            parents[i] = i;
+        }
+        count = parents.length;
+    }
+    
+    public int find(int i){
+        int parent = parents[i];
+        if(parent == i){
+            return i;
+        }else{
+            int root = find(parent);
+            parents[i] = root;
+            return root;
+        }
+    }
+    
+    public boolean union(int i, int j){
+        int r1 = find(i);
+        int r2 = find(j);
+        if(r1 != r2){
+            count--;
+            parents[r1] = r2;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}
