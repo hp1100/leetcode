c416417798c8952cbe335098b18c1d79b812e5fa
Huici Pan <hp1100@nyu.edu>
2020-08-01 02:11:50 -0700

    Update FenwickTree.java

--- templates/FenwickTree.java
+++ templates/FenwickTree.java
@@ -6,6 +6,17 @@ class FenwickTree {
         this.len = n;
         tree = new int[n + 1];
     }
+    
+    // O(n) 建树：
+    // 每一个节点的值是由所有与自己直接相连的儿子的值求和得到的。
+    // 因此可以倒着考虑贡献，即每次确定完儿子的值后，用自己的值更新自己的直接父亲。
+    public void init(int[] arr) {
+        for (int i = 1; i <= len; i++) {
+            tree[i] += arr[i - 1];
+            int j = i + lowbit(i);
+            if (j <= len) tree[j] += tree[i];
+        }
+    }
 
     /**
      * 单点更新：将 index 这个位置 + delta
