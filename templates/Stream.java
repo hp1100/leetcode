f4230a0b9d16a945aca98b15067a7125367d15e8
Huici Pan <hp1100@nyu.edu>
2020-11-02 23:46:21 -0800

    Update Stream.java

--- templates/Stream.java
+++ templates/Stream.java
@@ -12,3 +12,10 @@
                 .map(str -> str.split("/", -1)[3])
                 //.collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
                 .collect(Collectors.groupingBy(s -> s, Collectors.summingInt(s -> 1)));
+
+        // Smaple: LinkedList 1 -> 0 -> 1 to 101(demical:5)
+        public int getDecimalValue(ListNode head) {
+        return Stream.iterate(head, i -> i != null, i -> i.next)
+                     .map(i -> i.val)
+                     .reduce(0, (a, b) ->  a * 2 + b);
+        }
